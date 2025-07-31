a67d4bf16db8494f0af8eab763fc76fc6210064f
Harshal Mali <harshal.mali@globalpay.com>
2025-07-31 15:59:41 +0530

    random number generation function added

--- app/src/main/java/com/example/luckynumberapplication/secondActivity.java
+++ app/src/main/java/com/example/luckynumberapplication/secondActivity.java
@@ -1,6 +1,9 @@
 package com.example.luckynumberapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class secondActivity extends AppCompatActivity {
+
+    TextView textView ;
+    Button shareResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,23 @@ public class secondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        textView = findViewById(R.id.resultText);
+        shareResult = findViewById(R.id.shareResult);
     }
+
+
+
+
+    public int generateRandomNumber()
+    {
+        int upperLimit = 1000;
+
+        Random random = new Random();
+        return random.nextInt(upperLimit);
+    }
+
 }
